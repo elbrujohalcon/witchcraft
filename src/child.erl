570d0075fa263cbf27f00acd4151923fca5bb009
Brujo Benavides <elbrujohalcon@inaka.net>
2012-08-21 16:56:21 -0300

    New Tests

--- src/child.erl
+++ src/child.erl
@@ -2,8 +2,14 @@
 
 -extends(parent).
 
--export([module/0, a_value/0]).
+-record(?MODULE, {id :: integer(), name = "NN" :: string(), breed_id :: undefined | integer()}).
+
+-export([module/0, a_value/0, new/0, attribute_names/0]).
 
 module() -> ?MODULE.
 
 a_value() -> child_value.
+
+attribute_names() -> record_info(fields, ?MODULE).
+
+new() -> #?MODULE{}.
--- src/parent.erl
+++ src/parent.erl
@@ -1,6 +1,6 @@
 -module(parent).
 
--export([run/0, module/0, a_value/0]).
+-export([run/0, module/0, a_value/0, print/1]).
 
 run() ->
   io:format("Module: ~p~nA Value: ~p~n", [module(), a_value()]).
@@ -8,3 +8,8 @@ run() ->
 module() -> ?MODULE.
 
 a_value() -> parent_value.
+
+print(ChildTuple) ->
+	[Name | Fields] = tuple_to_list(ChildTuple),
+	FieldNames = Name:attribute_names(),
+	io:format("INSERT INTO ~s ~p VALUES ~p~n", [Name, FieldNames, Fields]).
