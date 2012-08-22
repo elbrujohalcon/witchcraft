4725f2482aceb0d3b68b7ff7331ad7fd90ce3263
Brujo Benavides <elbrujohalcon@inaka.net>
2012-08-22 11:00:03 -0300

    Jay ets solution

--- src/g.erl
+++ src/g.erl
@@ -6,7 +6,7 @@
 -record(state, {i :: pos_integer(), n :: pos_integer()}).
 
 %% api
--export([start/1, count/1, count_many/2, avg_time/2]).
+-export([start/1, count/1, start_many/2, count_many/2, avg_time/2, avg_times/1]).
 
 %% gen_server callbacks
 -export([init/1, terminate/2, code_change/3, handle_call/3, handle_cast/2, handle_info/2]).
@@ -15,31 +15,47 @@
 %%% API
 %%%===================================================================
 -spec start(pos_integer()) -> [{ok, pid()}].
-start(N) -> [gen_server:start({local, list_to_atom(?MODULE_STRING ++ integer_to_list(I))}, ?MODULE, {I, N}, []) || I <- lists:seq(1, N)].
+start(N) ->
+  try ets:delete(wpool) catch _Class:_Error -> noop end,
+  ets:new(wpool, [public, named_table, ordered_set, {read_concurrency, true}]),
+  ets:insert(wpool, {'g-count', N}),
+  [gen_server:start({local, list_to_atom(?MODULE_STRING ++ integer_to_list(I))}, ?MODULE, {I, N}, []) || I <- lists:seq(1, N)].
 
--spec count(ask | test) -> {pos_integer(), pos_integer()}.
+-spec count(ask | test | ets) -> {pos_integer(), pos_integer()}.
 count(ask) ->
-  P = list_to_atom(?MODULE_STRING "1"),
-  timer:tc(fun() -> gen_server:call(P, count) end);
+  timer:tc(fun() -> gen_server:call(g1, count) end);
+count(ets) ->
+  timer:tc(fun() -> [{'g-count', N}|_] = ets:lookup(wpool, 'g-count'), N end);
 count(test) ->
   timer:tc(fun() -> count(?MODULE_STRING, 1) end).
 
 count(Sup, N) ->
-    case erlang:whereis(list_to_atom(Sup ++ integer_to_list(N))) of
-        undefined -> N - 1;
-        _Pid -> count(Sup, N + 1)
-    end.
+  case erlang:whereis(list_to_atom(Sup ++ integer_to_list(N))) of
+      undefined -> N - 1;
+      _Pid -> count(Sup, N + 1)
+  end.
 
--spec count_many(pos_integer(), ask | test) -> ok.
+-spec count_many(pos_integer(), ask | test | ets) -> [pid()].
 count_many(N, Type) ->
   [spawn(fun() -> timer:sleep(N), io:format("~p (~p): ~p~n", [Type, I, count(Type)]) end) || I <- lists:seq(1, N)].
 
--spec avg_time(pos_integer(), ask | test) -> float().
+-spec launch_many(pos_integer(), ask | test | ets) -> [pid()].
+launch_many(N, Type) ->
+  [spawn(fun() -> receive start -> io:format("~p (~p): ~p~n", [Type, I, count(Type)]) end end) || I <- lists:seq(1, N)].
+
+-spec start_many(pos_integer(), ask | test | ets) -> ok.
+start_many(N, Type) ->
+  [Pid ! start || Pid <- launch_many(N, Type)].
+
+-spec avg_time(pos_integer(), ask | test | ets) -> float().
 avg_time(N, Type) ->
   Self = self(),
-  [spawn(fun() -> timer:sleep(N), Self ! count(Type) end) || _ <- lists:seq(1, N)],
-  Rs = [receive {T, _} -> T end || _ <- lists:seq(1, N)],
+  [Pid ! start || Pid <- [spawn(fun() -> receive start -> Self ! count(Type) end end) || _ <- lists:seq(1,N)]],
+  Rs = [receive {T, _} -> T end || _ <- lists:seq(1,N)],
   lists:sum(Rs) / length(Rs).
+
+-spec avg_times(pos_integer()) -> {ETS::float(), Test::float(), Ask::float()}.
+avg_times(N) -> {avg_time(N, ets), avg_time(N, test), avg_time(N, ask)}.
 
 %%%===================================================================
 %%% init, terminate, code_change, info callbacks
