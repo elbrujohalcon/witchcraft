0b772d01c815ccf45c730c12e682bab956e5a25c
Brujo Benavides <elbrujohalcon@inaka.net>
2016-10-12 12:12:00 -0300

    'Reduce git module'

--- src/git.erl
+++ src/git.erl
@@ -2,17 +2,9 @@
 
 -export(['$handle_undefined_function'/2]).
 
--spec '$handle_undefined_function'(atom(), [any()]) -> any().
 '$handle_undefined_function'(Func, Args) ->
-  Git = git(),
   GitCommand = atom_to_list(Func),
   GitArgs = [io_lib:format("~p ", [Arg]) || Arg <- Args],
-  Command = [Git, $\s, GitCommand, $\s, GitArgs],
+  Command = ["git ", GitCommand, $\s, GitArgs],
   Output = os:cmd(Command),
   io:format("~s~n", [Output]).
-
-git() ->
-  case os:find_executable("git") of
-    false -> throw(enogit);
-    Path -> Path
-  end.
